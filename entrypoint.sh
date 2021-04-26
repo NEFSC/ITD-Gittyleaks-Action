a8a321d20cbe8462d5a6c6f195a8ecad81622ede
NEFSC GitHub Service Account <74726438+NEFSC-SVC-GitHub@users.noreply.github.com>
2021-04-26 16:27:22 -0400

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /github/workspace/ 
 
-echo gittyleaks --find-anything --verbose --no-banner
+gittyleaks --find-anything --verbose --no-banner
 
 # if result = 'No matches.' ; then
 #  echo "Game over!"
