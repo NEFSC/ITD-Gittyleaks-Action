78fc5b296fde0a88c504888b41d52a6511bee6ba
David_Chevrier <David.Chevrier@NOAA.gov>
2021-04-27 11:26:46 -0400

    changed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,24 @@ cd /github/workspace/
 
 echo gittyleaks --find-anything --verbose --no-banner
 
-result = gittyleaks --find-anything --verbose --no-banner
+CAPTURE_OUTPUT=$(gittyleaks --find-anything --verbose --no-banner)
 
-echo result
-
+if [ $? -eq 1 ]
+then
+  GITTYLEAKS_RESULT=$(echo -e "\e[31m🛑 STOP! gittyleaks encountered leaks")
+  echo "$GITTYLEAKS_RESULT"
+  echo "::set-output name=exitcode::$GITTYLEAKS_RESULT"
+  echo "----------------------------------"
+  echo "$CAPTURE_OUTPUT"
+  echo "::set-output name=result::$CAPTURE_OUTPUT"
+  echo "----------------------------------"
+  exit 1
+else
+  GITTYLEAKS_RESULT=$(echo -e "\e[32m✅ SUCCESS! Your code is good to go!")
+  echo "$GITTYLEAKS_RESULT"
+  echo "::set-output name=exitcode::$GITTYLEAKS_RESULT"
+  echo "------------------------------------"
+fi
 # if result = 'No matches.' ; then
 #  echo "Game over!"
 #  exit 1
